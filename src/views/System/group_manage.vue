b939ff9e05602a340beabed1f094fe0bf71e1ac4
ud <123>
2019-05-22 16:05:54 +0800

    no message

--- src/views/System/group_manage.vue
+++ src/views/System/group_manage.vue
@@ -3,7 +3,6 @@
     <query-form :config="queryFormConfig" ref="queryForm"></query-form>
     <el-row>
       <el-button type="primary" @click="addOrgButton" size="small">新建组织</el-button>
-      <el-button type="primary" @click="addOrgButton" size="small">测试按钮</el-button>
     </el-row>
     <Tables
       :data="queryList"
